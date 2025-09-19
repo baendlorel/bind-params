e4dc53954fd9a72675ba5b63934fe88e3727c7b4
aldia_mechdream <futami16237@gmail.com>
2025-09-20 07:24:31 +0800

    init

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,37 @@
-declare const __IS_DEV__: boolean;
+type Chop<T extends any[], N extends number, Acc extends any[] = []> = Acc['length'] extends N
+  ? T
+  : T extends [infer Head, ...infer Rest]
+    ? Chop<Rest, N, [...Acc, Head]>
+    : [];
+
+type NParams<
+  Fn extends (...args: any[]) => any,
+  N extends number,
+  Acc extends any[] = [],
+> = Acc['length'] extends N
+  ? Acc
+  : Parameters<Fn> extends readonly [infer First, ...infer Rest]
+    ? Rest extends any[]
+      ? NParams<(...args: Rest) => any, N, [...Acc, First]>
+      : Acc
+    : Acc;
+
+type Params<Fn extends (...args: any[]) => any> =
+  | []
+  | NParams<Fn, 1>
+  | NParams<Fn, 2>
+  | NParams<Fn, 3>
+  | NParams<Fn, 4>
+  | NParams<Fn, 5>
+  | NParams<Fn, 6>
+  | NParams<Fn, 7>
+  | NParams<Fn, 8>
+  | NParams<Fn, 9>
+  | NParams<Fn, 10>
+  | NParams<Fn, 11>
+  | NParams<Fn, 12>
+  | NParams<Fn, 13>
+  | NParams<Fn, 14>
+  | NParams<Fn, 15>
+  | NParams<Fn, 16>
+  | Parameters<Fn>;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
+import { $define, $max } from './native.js';
+
 /**
+ * Creates a new function with bound leading arguments.
+ * - new function preserves `name` and `length` properties of the original function.
+ * - full type hints are provided.
+ *
+ * ```ts
+ * function oldFn(a: number, b: string, c: boolean): void {}
+ * const newFn = bindParams(oldFn, 42, 'hello'); // newFn: (c: boolean) => void
+ * ```
+ *
+ * @param fn the function to bind arguments to
+ * @param bound first N arguments to bind
+ * @returns a new function with bound leading arguments
+ *
  * __PKG_INFO__
  */
+export function bindParams<
+  T extends (...args: any[]) => any,
+  Bound extends Params<T> = [],
+  Remainder extends any[] = Chop<Parameters<T>, Bound['length']>,
+>(fn: T, ...bound: Bound & Partial<Parameters<T>>): (...args: Remainder) => ReturnType<T> {
+  const newFn = function (...args: any[]) {
+    return fn(...bound, ...args);
+  };
 
-if (typeof __IS_DEV__ === 'undefined') {
-  Reflect.set(globalThis, '__IS_DEV__', true);
+  $define(newFn, 'name', { value: fn.name, configurable: true });
+  $define(newFn, 'length', {
+    value: $max(0, fn.length - bound.length),
+    configurable: true,
+  });
+
+  return newFn;
 }
